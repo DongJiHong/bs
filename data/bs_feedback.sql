199b2a8723f34ddc39422be8367e394935457904
DongJiHong <1828536451@qq.com>
2023-03-20 16:13:54 +0800

    data

---
+++ data/bs_feedback.sql
@@ -0,0 +1,13 @@
+create table feedback
+(
+    id       int auto_increment
+        primary key,
+    user_id  int          not null,
+    score    int          not null,
+    feedback varchar(100) null
+);
+
+INSERT INTO bs.feedback (id, user_id, score, feedback)
+VALUES (1, 1, 9, '不错');
+INSERT INTO bs.feedback (id, user_id, score, feedback)
+VALUES (3, 1, 9, '不错');
